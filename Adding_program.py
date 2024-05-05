de192840f194ee3307660e080ff9b6857b7e2bf1
Mareeshwaran606 <168702698+Mareeshwaran606@users.noreply.github.com>
2024-05-05 15:28:30 +0530

    Create Adding_program.py

---
+++ Adding_program.py
@@ -0,0 +1,10 @@
+def chanted(num_1,num_2):
+    add_numbers= num_1+num_2
+    return add_numbers 
+    
+    
+    
+    
+user_num1 = int(input("Enter your first number : "))
+user_num2 = int(input("Enter your first : "))
+print("The add number is : ",chanted(num_1=user_num1,num_2=user_num2))
