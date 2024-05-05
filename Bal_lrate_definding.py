9df94d5979b4512663834d9184827e89fc3b4f30
Mareeshwaran606 <168702698+Mareeshwaran606@users.noreply.github.com>
2024-05-05 17:40:08 +0530

    Create Bal_lrate_definding.py

---
+++ Bal_lrate_definding.py
@@ -0,0 +1,10 @@
+def user(a,b):
+    file = a*b
+    return file
+    
+num1=int(input("how match balls? :"))
+num2=int(input("one ball rate : "))
+
+
+
+print("total ball rate : ",user(a=num1,b=num2))
