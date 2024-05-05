e1fe1c1e78fda0fe282b080c32690ea77e1546dc
Karthick <168961334+nagajothikarthick007@users.noreply.github.com>
2024-05-05 15:34:16 +0530

    Create Second _program.py

---
+++ Second _program.py
@@ -0,0 +1,7 @@
+def string_adding(str_1,str_2):
+    add_variable=str_1+str_2
+    return add_variable
+ 
+str_value_1=input("enter the string one value : ")
+str_value_2=input("enter the string second value : ")
+print ("the your string : ",string_adding(str_1=str_value_1,str_2=str_value_2))
