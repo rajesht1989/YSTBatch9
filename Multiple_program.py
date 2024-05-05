9eb1c7dc83ee17e29c4ca482fa9d8592e087afe3
Karthick <168961334+nagajothikarthick007@users.noreply.github.com>
2024-05-05 16:22:53 +0530

    Create Multiple_program.py

---
+++ Multiple_program.py
@@ -0,0 +1,10 @@
+def calculate_total_cost(price_per_item,quantity,tax_rate):
+    result = price_per_item*quantity
+    rate=result-tax_rate
+    return rate
+    
+    
+per=int(input ("how much price : "))
+qun=int(input ("how many quantity : "))
+tax=int(input("how much tax : "))
+print("it is total cast",calculate_total_cost(price_per_item=per,quantity=qun,tax_rate=tax))
