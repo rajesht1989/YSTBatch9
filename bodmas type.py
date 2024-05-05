c1c74a79e28cdcf1ffa96df5656af64dcb77379f
vishwa862005 <168962337+vishwa862005@users.noreply.github.com>
2024-05-05 17:18:13 +0530

    Update

---
+++ bodmas type.py
@@ -0,0 +1,14 @@
+def line(a,b,c,d,e,f,g,h,i,j):
+    var1 = (a+b)*(c-d)/(e+f)*(g-h)/(i+j)
+    return var1
+num1=int(input("first num :"))
+num2=int(("second num :"))
+num3=int(input("third num :"))
+num4=int(input("fourth num :"))
+num5=int(input("fifth num :"))
+num6=int(input("sixth num :"))
+num7=int(input("seventh num :"))
+num8=int(input("eighth num :"))
+num9=int(input("ninth num :"))
+num10=int(input("tenth num :"))
+print("fainal result :",line(a=num1,b=num2,c=num3,d=num4,e=num5,f=num6,g=num7,h=num8,i=num9,j=num10))
