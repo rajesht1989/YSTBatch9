ab75d9b69482650d426ac0870435c0f002d218ce
Karthick <168961334+nagajothikarthick007@users.noreply.github.com>
2024-05-05 17:52:19 +0530

    Create Multiple_program_1.py

---
+++ Multiple_program_1.py
@@ -0,0 +1,8 @@
+def ball_rate_finder(one_ball_rate, number_of_ball):
+    result = one_ball_rate * number_of_ball
+    return result
+   
+finding_value = int(input("Enter one ball rate: "))
+number_of_ball = int(input("Enter number of balls: "))
+
+print("Total rate:", ball_rate_finder(one_ball_rate=finding_value, number_of_ball=number_of_ball))
