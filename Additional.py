d4365d3d8ebf54d25ca76304745e1061b7cc8373
srivijay000 <168962037+srivijay000@users.noreply.github.com>
2024-05-05 17:21:41 +0530

    Create Additional.py

---
+++ Additional.py
@@ -0,0 +1,14 @@
+def ask_mark(a,b,c,d,e):
+   store=(a+b+c+d+e)
+  return store
+
+n1=int(input("what is your tamil mark")
+n2=int(input("whst is your english mark")
+n3=int(input("what is your maths mark")
+n4=int(input("what is your science mark")
+n5=int(input("what is your social mark")
+print(ask_mark(a=n1,b=n2,c=n3,d=n4,d=n5")
+     
+
+
+
