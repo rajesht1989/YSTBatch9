9a7f7da3a2ffd228513bf4b19d5467d145339eb5
srivijay000 <168962037+srivijay000@users.noreply.github.com>
2024-05-07 11:12:29 +0530

    Create Construction.py

---
+++ Construction.py
@@ -0,0 +1,10 @@
+def consentrate (a,b,c,d):
+  building=(a,b,c,d)
+  return building
+
+v9=int(input("memory is increased""
+v7=int(input("handel carefully")
+v5=int(input("work from building")
+v3=int(input("it is a job")
+
+print building (a=v9,b=7,c=5,d=3)
