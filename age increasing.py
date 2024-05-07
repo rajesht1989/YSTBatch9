414e8ad7cc77f57b93fa31a95bc8bef1ad0cbde2
vishwa862005 <168962337+vishwa862005@users.noreply.github.com>
2024-05-07 10:35:29 +0530

    Update

---
+++ age increasing.py
@@ -0,0 +1,10 @@
+def user(name,age):
+    var1 = age+10
+    var2 = var1,name
+    return var2
+num1=input("what is your name? :")
+num2=int(input("what is your age? :"))
+print("your name is :",user(name=num1,age=num2))
+
+
+    
