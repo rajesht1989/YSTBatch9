4ea48454979ac043312179086d8d09fa66bbfad6
srivijay000 <168962037+srivijay000@users.noreply.github.com>
2024-05-07 10:56:58 +0530

    Create Abtitude.py

---
+++ Abtitude.py
@@ -0,0 +1,10 @@
+def section(1,2,3,4):
+   appartment=(1,2,3,4)
+return appartment
+
+num2=int(input("there are many rooms")
+num4=int(input("there live in many people")
+num6=int(input("the designing appartment")
+num8=int(input("luxury appartment")
+
+         print appartment(1=num2,2=num4,3=num6,4=num8)
