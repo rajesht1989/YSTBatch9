5eb1e965b9b6725060ade2d2de8a1cdd272cf196
srivijay000 <168962037+srivijay000@users.noreply.github.com>
2024-05-07 10:27:40 +0530

    Create Division.py

---
+++ Division.py
@@ -0,0 +1,9 @@
+def model(a,b,c,d):
+  project=(a,b,c,d)
+  return project
+
+t1=int(input("your project model")
+t2=int(input("your project size")
+t3=int(input("your project level")
+t4=int(input("your project decision")
+       print project(a=t1,b=t2,c=t3,d=t4)
