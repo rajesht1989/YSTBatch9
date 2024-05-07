5edb7da772b4d5be29339a31d71e9ba094ceab08
srivijay000 <168962037+srivijay000@users.noreply.github.com>
2024-05-07 10:48:41 +0530

    Create eqaction.py

---
+++ eqaction.py
@@ -0,0 +1,10 @@
+def house(10,20,30,40):
+   people=10,20,30,40)
+return people
+
+p1=int(input("people live in house")
+p2=int(input("enjoy the house")
+p3=int(input("many members are live")
+p4=int(input("very brightness house")
+
+       print people(10=p1,20=p2,30=p3,40=p4)
