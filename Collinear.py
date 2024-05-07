eeae06968027432c618411074bf0d544b765271c
srivijay000 <168962037+srivijay000@users.noreply.github.com>
2024-05-07 10:39:22 +0530

    Create Collinear.py

---
+++ Collinear.py
@@ -0,0 +1,15 @@
+def place(1,2,3,4,5,6 ):
+   section=(1,2,3,4,5,6)
+return section
+
+y1=int(input("you got the place")
+y2=int(input("your forgot the section")
+y3=int(input("place in many distance of road")
+y4=int(input("that place is very heat")
+y5=int(input("night is very darkness")
+y6=int(input("you not forget the place")
+
+       print section(1=y1,2=y2,3=y3,4=y4,5=y5,6=y6)
+
+             
+             
