af9707bbfef4b54e01b4f2cc6ffcfd1e7b02cc91
srivijay000 <168962037+srivijay000@users.noreply.github.com>
2024-05-07 11:18:34 +0530

    Create Working.py

---
+++ Working.py
@@ -0,0 +1,11 @@
+def box(a,b,c,d,e):
+  pen=a,b,c,d,e)
+  return pen
+
+n1=int(input("pen have writing products")
+n2=int(input("full fill the ink")
+n3=int(input("very smooth writing")
+n4=int(input("it pen is nice")
+n5=int(input("it very super")
+
+       print pen(a=n1,b=n2,c=n3,d=n4,e=n5)
