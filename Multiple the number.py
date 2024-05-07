4a07a331a339062f0f7e8dec21a4562a67d378ca
srivijay000 <168962037+srivijay000@users.noreply.github.com>
2024-05-07 10:20:39 +0530

    Create Multiple the number.py

---
+++ Multiple the number.py
@@ -0,0 +1,9 @@
+def name(2,4,6,8):
+   age=(2,4,6,8)
+return age
+b1=int(input("your age")
+b2=int(input("your date")
+b3=int(input("your height")
+b4=int(input("your weight")
+    print age(2=b1,4=b2,6=b3,8=b4)
+  
