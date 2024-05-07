cdeda1a0e79e7eff8b7f3e48bfaa2917a025cce9
vishwa862005 <168962337+vishwa862005@users.noreply.github.com>
2024-05-07 11:23:14 +0530

    Update

---
+++ human details.py
@@ -0,0 +1,13 @@
+def it_my_detail(name,blood_group,age):
+    result=age+100
+    group=result,name
+    details=group,blood_group
+    return details
+    
+    
+your_name=input("enter your name : ")
+your_blood_group=input("enter your blood group : ")
+your_age=int(input("enter your age : "))
+print ("your details :",it_my_detail(name=your_name,blood_group=your_blood_group,age=your_age))
+
+
