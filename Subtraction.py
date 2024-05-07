7ccc7cb8314b1ca6b26b0e64feae17662df1942f
srivijay000 <168962037+srivijay000@users.noreply.github.com>
2024-05-07 10:01:18 +0530

    Create Subtraction.py

---
+++ Subtraction.py
@@ -0,0 +1,10 @@
+def school(a,b,c,d,e):
+   class=a,b,c,d,e
+  return class
+v1=int(input("many class in school")
+v2=int(input("many student in school")
+v3=int(input("many table in school")
+v4=int(input("many window in school"
+v5=int(input("many teachers in school")
+
+print school(a=v1,b=v2,c=v3,d=v4,e=v5)
