d51bf4e96c22f49b0a2dc296fc60513e824c2e25
srivijay000 <168962037+srivijay000@users.noreply.github.com>
2024-05-07 11:05:07 +0530

    Create Create.py

---
+++ Create.py
@@ -0,0 +1,9 @@
+def create(1,3,5,7):
+   image=(1,3,5,7 )
+return image
+u1=int(input("image in photo")
+u2=int(input("it is naturally")
+u3=int(input("it createvelly")
+u4=int(input("it is own of photo")
+
+       print image(1=u1,3=u2,5=u3,7=u4)
