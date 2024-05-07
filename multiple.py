4abaa7f2851afb0c13290fc9dd49be064c4a1150
vishwa862005 <168962337+vishwa862005@users.noreply.github.com>
2024-05-07 10:50:30 +0530

    Update

---
+++ multiple.py
@@ -0,0 +1,8 @@
+def user(a,b):
+    num1=a**3
+    num2=num1%b
+    return 
+
+var1=int(input("your first value? :"))
+var2=int(input("your second value? ?:"))
+print("expected result :",user(a=var1,b=var2))
