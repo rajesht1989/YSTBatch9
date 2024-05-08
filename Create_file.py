b2202bb04b8693fe422a880a48457503d51805bb
srivijay000 <168962037+srivijay000@users.noreply.github.com>
2024-05-08 10:32:33 +0530

    Create Create_file.py

---
+++ Create_file.py
@@ -0,0 +1,6 @@
+def create_circle_area(r):
+    area=3.14*(r**2)
+    return area
+input_value1=float(input("enter your number: "))
+input_value2=int(input("enter your number: "))
+print("expect value: ",create_circle_area(r=input_value1))
