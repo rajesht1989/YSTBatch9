6c284694a358b09afc6774e43e07215070227927
KARTHICK <168961334+nagajothikarthick007@users.noreply.github.com>
2024-05-08 14:59:10 +0530

    Create calculat_circle_circumference.py

---
+++ calculat_circle_circumference.py
@@ -0,0 +1,7 @@
+def calculat_circle_circumference(diameter):
+    circumference=3.14*diameter
+    return circumference
+    
+diameter_input_value=int(input("enter the diameter value : "))
+
+print ("The circle circumference value is : ",calculat_circle_circumference(diameter=diameter_input_value))
