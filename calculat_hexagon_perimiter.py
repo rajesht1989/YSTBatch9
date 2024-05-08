a4c8501fe0cc2631b68babb84f2df509380db52c
KARTHICK <168961334+nagajothikarthick007@users.noreply.github.com>
2024-05-08 15:05:02 +0530

    Create calculat_hexagon_perimiter.py

---
+++ calculat_hexagon_perimiter.py
@@ -0,0 +1,6 @@
+def calculat_hexagon_perimiter(side_lenght):
+    hexagon_perimiter=6*side_lenght
+    return hexagon_perimiter
+    
+side_lenght_input=int(input("enter the side lenght value : "))
+print ("hexagon perimiter value is : ",calculat_hexagon_perimiter(side_lenght=side_lenght_input))
