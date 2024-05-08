38d17a9536bbbbc0b39d8b89882e598253f157c6
Karthick <168961334+nagajothikarthick007@users.noreply.github.com>
2024-05-08 11:11:20 +0530

    Create calculat_rectangle_perimiter.py

---
+++ calculat_rectangle_perimiter.py
@@ -0,0 +1,9 @@
+def calculat_rectangle_perimiter(length,width):
+    Perimiter=2*(length+width)
+    return Perimiter
+    
+length_input_value=int(input("enter the length value : "))
+width_input_value=int(input("enter the width value : "))
+
+
+print ("calculat rectangle permiter value is : ",calculat_rectangle_perimiter(length=length_input_value,width=width_input_value) )
