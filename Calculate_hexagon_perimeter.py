c09e5008aac92fbf06646822485f873dde0dcdde
srivijay000 <168962037+srivijay000@users.noreply.github.com>
2024-05-08 15:06:30 +0530

    Create Calculate_hexagon_perimeter.py

---
+++ Calculate_hexagon_perimeter.py
@@ -0,0 +1,6 @@
+def calculate_hexagon_perimeter(s,l):
+    perimeter=6*s
+    return perimeter
+percentage1=int(input("enter your number1:"))
+percentage2=int(input("enter your number2:"))
+print("final result",calculate_hexagon_perimeter(s=percentage1,l=percentage2))
