2d5a8158c9329b0818c1a23df717a5e5f4e3d32d
Karthick <168961334+nagajothikarthick007@users.noreply.github.com>
2024-05-08 11:14:17 +0530

    Create calculate_equilateral_triangle_perimeter.py

---
+++ calculate_equilateral_triangle_perimeter.py
@@ -0,0 +1,7 @@
+def calculate_equilateral_triangle_perimeter(side_length):
+     perimeter=3*side_length
+     return perimeter
+     
+side_length_input=float(input("enter the side length : "))
+ 
+print ("The calculate equilateral triangle perimeter" ,calculate_equilateral_triangle_perimeter(side_length=side_length_input))
