1efff6d2c64ac67a53e119cb8942ac78ff8612e5
srivijay000 <168962037+srivijay000@users.noreply.github.com>
2024-05-08 14:42:56 +0530

    Create Calculate_parallellogram_area.py

---
+++ Calculate_parallellogram_area.py
@@ -0,0 +1,6 @@
+def calculate_parallelogram_area(b,h):
+    area=b*h
+    return area
+first_input=int(input("Enter your first number : "))
+second_input=int(input("Enter your second number : "))
+print("Fainal Parallelogram area : ",calculate_parallelogram_area(b=first_input,h=second_input))
