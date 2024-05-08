bde77ab742dfd4f228ec44e662fa686f03678c15
srivijay000 <168962037+srivijay000@users.noreply.github.com>
2024-05-08 12:28:31 +0530

    Create Calculate_rectangle_prism_volume.py

---
+++ Calculate_rectangle_prism_volume.py
@@ -0,0 +1,7 @@
+def calculate_retangle_prism_volume(L,W,H):
+    volume=L*W*H
+    return volume
+input_1value=int(input("enter your number1"))
+input_2value=int(input("enter your number2"))
+input_3value=int(input("enter your number3"))
+print("final result",calculate_retangle_prism_volume(L=input_1value,W=input_2value,H=input_3value))
