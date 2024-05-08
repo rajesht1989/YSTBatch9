ccda2dc332770e4ab7ba13beb1269a769ba4afc4
srivijay000 <168962037+srivijay000@users.noreply.github.com>
2024-05-08 15:07:41 +0530

    Create Calculate_ellipse_area.py

---
+++ Calculate_ellipse_area.py
@@ -0,0 +1,6 @@
+def calculate_ellipse_area(a,b):
+    area=3.14*a*b
+    return area
+input_first_number=int(input("Enter your first number : "))
+=int(input("Enter your second number : "))
+print("Fainal ellipse area : ",calculate_ellipse_area(a=input_first_number,b=input_second_number))
