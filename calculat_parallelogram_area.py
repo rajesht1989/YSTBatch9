3d8ee89b321b611754ac4eb2681f62df630d3a4a
KARTHICK <168961334+nagajothikarthick007@users.noreply.github.com>
2024-05-08 15:01:56 +0530

    Create calculat_parallelogram_area.py

---
+++ calculat_parallelogram_area.py
@@ -0,0 +1,8 @@
+def calculat_parallelogram_area(base,height):
+    area=base*height
+    return area
+    
+base_value_input=int(input("enter the base value : "))
+height_value_input=float(input("enter the height value : "))
+
+print ("calculat parallelogram area : ", calculat_parallelogram_area(base=base_value_input,height=height_value_input))
