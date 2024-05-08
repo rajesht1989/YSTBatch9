960de8190ec07ada57dc96f8eeb9d3f4622d46a1
srivijay000 <168962037+srivijay000@users.noreply.github.com>
2024-05-08 12:01:08 +0530

    Create Calculate_rectangle_perimeter.py

---
+++ Calculate_rectangle_perimeter.py
@@ -0,0 +1,7 @@
+def calculate_rectangle_perimeter(L,W):
+    perimeter=2*(L+W)
+    return perimeter
+  
+input_number1=int(input("enter your 1number"))
+input_number2=int(input("enter your 2number"))
+print("final result",calculate_rectangle_perimeter(L=input_number1,W=input_number2))
