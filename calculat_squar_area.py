26539893ed890489440a163d682d957afbb9c12f
KARTHICK <168961334+nagajothikarthick007@users.noreply.github.com>
2024-05-08 15:00:13 +0530

    Create calculat_squar_area.py

---
+++ calculat_squar_area.py
@@ -0,0 +1,7 @@
+def calculat_squar_area(side_lenght):
+    squar_area=side_lenght**2
+    return squar_area
+    
+input_side_lenght=float(input("enter the side lenght : "))
+
+print ("The squar area value is : " , calculat_squar_area(side_lenght=input_side_lenght))
