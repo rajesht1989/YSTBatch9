5129a5cdce2f80f8f18ddfe122f89f533a9838c1
Mareeshwaran606 <168702698+Mareeshwaran606@users.noreply.github.com>
2024-05-09 22:35:05 +0530

    Create Square_area_calculate.py

---
+++ Square_area_calculate.py
@@ -0,0 +1,6 @@
+def calculate_square_area(s):
+    square_area_formula = s**2
+    return square_area_formula
+    
+user_square_value=float(input("Enter your side length value : "))
+print("It is your square said length value : ",calculate_square_area(s=user_square_value))
