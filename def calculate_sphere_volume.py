f8d6efe61454fc93b85df1f76696ab554239dd0c
srivijay000 <168962037+srivijay000@users.noreply.github.com>
2024-05-09 10:55:33 +0530

    Create def calculate_sphere_volume.py

---
+++ def calculate_sphere_volume.py
@@ -0,0 +1,5 @@
+def calculate_sphere_volume(r):
+    volume=(4/3)*3.14*r^3
+    return volume
+input_1number=int(input("enter your number"))
+print ("fainal value",calculate_sphere_volume(r=input_1number))
