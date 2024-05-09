bad7498fcecf0cb23a483fe93521a2a9d209d138
srivijay000 <168962037+srivijay000@users.noreply.github.com>
2024-05-09 10:24:16 +0530

    Create circle_circumference.py

---
+++ circle_circumference.py
@@ -0,0 +1,5 @@
+def calculate_circle_circumference(d):
+    circumference=3.14*d
+    return circumference
+input_number=int(input("Enter your number : "))
+print("Fainal circumference",calculate_circle_circumference(d=input_number))
