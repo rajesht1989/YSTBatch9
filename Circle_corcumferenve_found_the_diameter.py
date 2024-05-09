ae8bfa402ca2e558e1c59c405e97734ffc9b344f
Mareeshwaran606 <168702698+Mareeshwaran606@users.noreply.github.com>
2024-05-09 22:37:57 +0530

    Create Circle_corcumferenve_found_the_diameter.py

---
+++ Circle_corcumferenve_found_the_diameter.py
@@ -0,0 +1,8 @@
+def calculate_circle_circumference(d):
+    π=3.14
+    cricle_diameter_formula=π*d
+    return cricle_diameter_formula
+    
+user_diameter_value=float(input("enter your diameter vaule : "))
+
+print("It is your circle diameter value : ",calculate_circle_circumference(d=user_diameter_value))
