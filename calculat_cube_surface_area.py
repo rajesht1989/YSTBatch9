d5ae7f124d059fd310b693beeae1be50be26e47c
Mareeshwaran606 <168702698+Mareeshwaran606@users.noreply.github.com>
2024-05-10 07:21:58 +0530

    Create calculat_cube_surface_area.py

---
+++ calculat_cube_surface_area.py
@@ -0,0 +1,6 @@
+def calculat_cube_surface_area(side_lenght):
+    surface_area=6*(side_lenght**2)
+    return surface_area
+    
+input_side_lenght=int(input("enter the side lenght : "))
+print ("calculat cube surface area : ",calculat_cube_surface_area(side_lenght=input_side_lenght))
