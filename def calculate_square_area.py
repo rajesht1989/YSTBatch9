a465b7842658ca5cc2129b6f894681b7ce5b1de0
vishwa862005 <168962337+vishwa862005@users.noreply.github.com>
2024-05-11 09:42:02 +0530

    Update calculate_square_area.py

--- def calculate_square_area.py
+++ def calculate_square_area.py
@@ -1,5 +1,5 @@
-def calculate_square_area(s):
-    area=s**2
+def calculate_square_area(S):
+    area=S**2
     return area
 input_number=int(input("Entet your number : "))
-print("Fainal square area : ",calculate_square_area(s=input_number))
+print("Fainal square area : ",calculate_square_area(S=input_number))
