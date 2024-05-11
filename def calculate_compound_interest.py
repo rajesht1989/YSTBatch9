595640da68d37f8f062ef5ec1860904517e9ad45
vishwa862005 <168962337+vishwa862005@users.noreply.github.com>
2024-05-11 09:43:48 +0530

    Update calculate_compound_interest.py

--- def calculate_compound_interest.py
+++ def calculate_compound_interest.py
@@ -1,8 +1,8 @@
-def calculate_compound_interest(p,r,t):
+def calculate_compound_interest(P,R,T):
     compound_interest=p*(1+r/100)**t-p
     return compound_interest
 input_1value=int(input("enter your 1number"))
 input_2value=int(input("enter your 2number"))
 input_3value=int(input("enter your 3number"))
 
-print("final result",calculate_compound_interest(p=input_1value,r=input_2value,t=input_3value))
+print("final result",calculate_compound_interest(P=input_1value,R=input_2value,T=input_3value))
