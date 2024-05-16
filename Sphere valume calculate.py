2f9957c11263bbe83cbf93fb1525c6534cd50603
Muthukumar4407 <muthukumar04042007@gmail.com>
2024-05-16 11:04:17 +0530

    Update Sphere_valume_calculate.py

--- Sphere valume calculate.py
+++ Sphere valume calculate.py
@@ -3,4 +3,4 @@ def calculate_speare_volume(radius):
     return volume
 given_radius=float(input("enter the radius:"))
 result=calculate_speare_volume(radius=given_radius)
-print ("The volume of the speare is:", result)
+print ("The volume of the speare is: ", result)
