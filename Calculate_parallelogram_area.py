95e228ecde5179068ce25a3c237e58a1d24a4b2c
Muthukumar4407 <muthukumar04042007@gmail.com>
2024-05-16 12:27:47 +0530

    Calculate area of the parallelogram

---
+++ Calculate_parallelogram_area.py
@@ -0,0 +1,7 @@
+def calculate_parallelogram_area(base, height):
+    area=height*base
+    return area 
+Base=float(input ("enter the value:"))
+Height=float(input ("enter the value:"))
+result=calculate_parallelogram_area(base=Base, height=Height)
+print ("Volume of the cone is:", result)
