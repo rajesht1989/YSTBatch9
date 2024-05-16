732a4473731b1d94b01e5493a0ad63c8d7f3e406
Muthukumar4407 <muthukumar04042007@gmail.com>
2024-05-16 12:09:35 +0530

    Calculate circumference of the circle

---
+++ Calculate_circle_cumference.py
@@ -0,0 +1,6 @@
+def calculate_circle_circumference(diameter):
+    circumference=3.14*diameter
+    return circumference
+Diameter=float(input ("enter the value:"))
+result=calculate_circle_circumference(diameter=Diameter)
+print ("Circumference of the circle is:", result)
