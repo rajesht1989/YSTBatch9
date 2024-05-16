aff6c46e2a1ddd9c532e77738ee4a99619cd477d
Muthukumar4407 <muthukumar04042007@gmail.com>
2024-05-16 12:05:43 +0530

    Calculate the perimeter of Equilateral Triangle

---
+++ Calculate_equilateral_triangle_perimeter.py
@@ -0,0 +1,6 @@
+def calculate_equilateral_triangle_perimeter(side_length):
+    perimeter=3*side_length
+    return perimeter
+length=float(input("enter the value:"))
+result=calculate_equilateral_triangle_perimeter(side_length=length)
+print ("perimeter of Equilateral Triangle : ", result)
