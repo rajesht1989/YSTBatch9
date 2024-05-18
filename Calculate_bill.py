ff66ef2f3a546530f712012f5c667cec3270a9b7
Muthukumar4407 <muthukumar04042007@gmail.com>
2024-05-18 10:30:38 +0530

    Calculate the bill amount

---
+++ Calculate_bill.py
@@ -0,0 +1,5 @@
+def calculate_bill(price,quantity):
+    return price*quantity
+Price=float (input ("enter the amount:"))
+Quantity=int(input ("enter the quantity:"))
+print (calculate_bill(price=Price,quantity=Quantity))
