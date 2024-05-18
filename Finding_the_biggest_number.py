865bc00b8d543aefb84c6ca7488318b7fa46d334
Muthukumar4407 <muthukumar04042007@gmail.com>
2024-05-18 10:20:45 +0530

    Finding the biggest number

---
+++ Finding_the_biggest_number.py
@@ -0,0 +1,8 @@
+def max_of_2(num1,num2):
+    if num1>num2:
+        return ("number 1 is biggest number")
+    else:
+        return ("number 2 is biggest number ")
+number_1=float (input ("enter the first number : "))
+number_2=float (input ("enter the second number : "))
+print (max_of_2(num1=number_1,num2=number_2))
