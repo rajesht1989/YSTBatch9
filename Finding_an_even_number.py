48a0664308f8ec60e60c837cec15864b6bdee97e
Muthukumar4407 <muthukumar04042007@gmail.com>
2024-05-18 09:58:36 +0530

    If given number is even return true else return false

---
+++ Finding_an_even_number.py
@@ -0,0 +1,7 @@
+def is_even(number):
+    if (number%2==0):
+        return ("it is true ")
+    else:
+        return ("it is false" )
+given_number=int(input ("enter the number: "))
+print (is_even(number=given_number))
