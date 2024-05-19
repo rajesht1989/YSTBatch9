807e33aa76319711b136c04a88ca01b14eb2d181
Muthukumar4407 <muthukumar04042007@gmail.com>
2024-05-19 11:50:56 +0530

    checking the Gmail id

---
+++ Gmail_id_checking.py
@@ -0,0 +1,7 @@
+def find_gmail(mail_id):
+    if mail_id=="muthu@gamil.com":
+        return ("your 🆔 is correct")
+    else:
+        return ("your 🆔 is incorrect ")
+gmail_id=input ("enter your gmail ID :")
+print (find_gmail(mail_id=gmail_id))
