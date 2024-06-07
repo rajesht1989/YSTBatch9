19151d32d2e105e10d0682e575c92286d01f00df
Maree <168702698+Mareeshwaran606@users.noreply.github.com>
2024-06-07 18:03:49 +0530

    Create check_armstrong(number).py

---
+++ check_armstrong(number).py
@@ -0,0 +1,11 @@
+def check_armstrong(number):
+    variable=[]
+    num=number
+    sum=0
+    while num > 0:
+        sum=(num%10)**3
+        num=int(m/10)
+        variable.append()
+num_input=int(input()) 
+
+print(check_armstrong(number=num_input))
