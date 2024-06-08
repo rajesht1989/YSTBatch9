3479ca570cebdf46bba538f1e06e89d0ae1df1ed
Muthukumar4407 <muthukumar04042007@gmail.com>
2024-06-08 10:51:12 +0530

    Create Check_prime_number

---
+++ Check_prime_number.py
@@ -0,0 +1,10 @@
+def check_prime_number(num):
+    bool=true 
+    for i in range (2,num):
+        if(num%i==0):
+            print ("it is not prime number")
+            bool=false 
+            break
+        if(prime):
+            print ("it is prime number ")
+(check_prime_number(num=6))
