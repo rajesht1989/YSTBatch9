689433898365661bd0f34eb97296eb9b62e6cc21
Muthukumar4407 <muthukumar04042007@gmail.com>
2024-06-08 11:26:18 +0530

    Fibonacci_sequence

---
+++ Fibonacci_sequence.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+    fib_sequence = []
+    a, b = 0, 1
+    for _ in range(n):
+        fib_sequence.append(a)
+        a, b = b, a + b
+    return fib_sequence
+n=int(input("enter a last number: "))
+print (fibonacci(n))
